3f81bd03747c46bb68521d91ce6ca4ab8f09b037
Bishal koirala <bishal123koirala@gmail.com>
2024-05-05 21:00:59 +0545

    done

--- Web-.sql
+++ Web-.sql
@@ -49,6 +49,109 @@ FROM student
 GROUP BY city
 ORDER BY avg(marks); 
 
+-- another practise question
+Use college;
+
+-- creating table for payment  
+CREATE TABLE payment(
+	customer_id INT PRIMARY KEY,
+    customer VARCHAR(50),
+    mode VARCHAR(20),
+    city VARCHAR(30)
+);
+
+-- now insert a data on it 
+INSERT INTO payment
+(customer_id, customer, mode, city)
+VALUES
+(101, "Olivia Barrett", "Netbanking", "Portland"),
+(102, "Ethan Sinclair", "Credit Card", "Miami"),
+(103, "Maya Hernandez", "Credit Card", "Settle"),
+(104, "Liam Donovan", "Netbanking", "Denver"), 
+(105, "Sophia Nguyen", "Credit Card", "New Orleans"), 
+(106, "Caleb Foster", "Debit Card", "Mineapolis"), 
+(107, "Ava Patel", "Debit Card", "Phoenix"), 
+(108, "Lucas Carter", "Netbanking", "Boston"), 
+(109, "Isabella Martinez", "Netbanking", "Nashville"), 
+(110, "Jackson Brooks", "Credit Card", "Boston");
+
+--  Using group by 
+SELECT mode , COUNT(customer)
+FROM payment
+GROUP BY mode
+ORDER BY COUNT(customer) ASC;
+
+-- using group by basis in grade 
+SELECT grade, COUNT(name)
+FROM student
+GROUP BY grade
+ORDER BY grade;
+
+
+-- Having clause use with grouping and applicable with grouping
+SELECT city
+FROM student
+WHERE grade = "A"
+GROUP BY city
+HAVING MAX(marks) > 90
+ORDER BY city;
+
+--  First remove a safe mode into off for updata or changes 
+SET  SQL_SAFE_UPDATES = 0;
+
+SELECT * FROM student;
+
+UPDATE student
+SET grade = "O"
+WHERE grade = "A";
+
+UPDATE student
+SET grade = "B"
+WHERE marks + 1;
+
+SELECT * FROM student;
+
+-- DELETE CLAUSE 
+DELETE FROM student
+WHERE marks < 33; 
+
+SELECT * FROM student;
+
+-- Revisiting Foreign key
+CREATE TABLE dept (
+    id INT PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+CREATE TABLE teacher(
+id INT PRIMARY KEY,
+name VARCHAR(50),
+dept_id int,
+FOREIGN KEY (dept_id) REFERENCES dept(id)
+-- cascading foreign key  TO CHANGES 
+ON UPDATE CASCADE
+ON DELETE CASCADE
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
